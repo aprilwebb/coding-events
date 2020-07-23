287c7aba1877b38cef9a9c1f87d6b0d69dfdf708
April Webb <aprilewebb@gmail.com>
2020-07-23 18:51:59 -0400

    Added fragment

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Chris Bay
@@ -17,11 +19,19 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
+//        List<String> events = new ArrayList<>();
+//        events.add("Code With Pride");
+//        events.add("Strange Loop");
+//        events.add("Apple WWDC");
+//        events.add("SpringOne Platform");
+//        model.addAttribute("events", events);
+//        return "events/index";
+        Map<String, String> events = new HashMap<>();
+        events.put("Women Who Code", "We help you build the skills you need to raise your " +
+                "professional profile and achieve greater career success.");
+        events.put("USF Summer Tech Camps", "The world's #1 STEM summer camp");
+        events.put("Tampa Beginner Programmers", "This is a group for anyone who has never " +
+                "written a computer program or is in the very early stages of learning. ");
         model.addAttribute("events", events);
         return "events/index";
     }
