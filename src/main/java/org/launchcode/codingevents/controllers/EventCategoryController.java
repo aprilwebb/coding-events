662d5f22980ad94465da5753151c7809a27bb902
April Webb <aprilewebb@gmail.com>
2020-08-17 19:20:09 -0400

    Created AbstractEntity class, added delete functionality

--- src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -51,4 +48,23 @@ public class EventCategoryController {
         return "redirect:";
     }
 
+    @GetMapping("delete")
+    public String displayDeleteEventCategoryForm(Model model) {
+        model.addAttribute("title", "Delete Category");
+        model.addAttribute("categories", eventCategoryRepository.findAll());
+        return "eventCategories/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventCategoryIds) {
+
+        if (eventCategoryIds != null) {
+            for (int id : eventCategoryIds) {
+                eventCategoryRepository.deleteById(id);
+            }
+        }
+
+        return "redirect:";
+
+    }
 }
